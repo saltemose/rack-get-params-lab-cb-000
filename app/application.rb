edb6035bf5f99044e8b1666df7ceed9f1fb9e849
Sean Altemose <sean@seanaltemose.com>
2018-06-26 18:44:45 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,4 +28,26 @@ class Application
       return "Couldn't find #{search_term}"
     end
   end
+
+  if req.path.match(/cart/)
+    @@cart.each do |item|
+      resp.write "#{item}\n"
+    end
+  elsif req.path.match(/search/)
+    search_term = req.params["q"]
+    resp.write handle_search(search_term)
+  else
+    resp.write "Path Not Found"
+  end
+
+  resp.finish
+end
+
+def handle_search(search_term)
+  if @@items.include?(search_term)
+    return "#{search_term} is one of our items"
+  else
+    return "Couldn't find #{search_term}"
+  end
+end
 end
