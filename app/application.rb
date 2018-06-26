967c65a3ebb2fcac471ee058a112314e1ffe37cf
Sean Altemose <sean@seanaltemose.com>
2018-06-26 18:51:37 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -22,6 +22,7 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
+      else resp.write "We don't have that item"
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
